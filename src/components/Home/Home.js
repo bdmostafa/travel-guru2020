cacd1a3437d8358c41122cae78f45afdb20cc1fa
bdmostafa <mdmostafaalmahmud@gmail.com>
2020-09-16 11:08:07 +0600

    header material ui

---
+++ src/components/Home/Home.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Header from '../Header/Header';
+
+const Home = () => {
+    return (
+        <div className="homeBg">
+            <Header />
+        </div>
+    );
+};
+
+export default Home;
